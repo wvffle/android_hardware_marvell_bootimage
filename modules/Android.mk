855cafbbd60307cf0804d3ba6e2318428620bc69
Nemirtingas <nanaki89@hotmail.fr>
2018-09-14 17:57:35 +0200

    debug cm12

--- modules/Android.mk
+++ modules/Android.mk
@@ -1,4 +1,8 @@
+ifeq (0,1)
+
 hardware_modules := gralloc hwcomposer audio nfc nfc-nci local_time \
 	power usbaudio audio_remote_submix camera consumerir sensors vibrator \
 	tv_input fingerprint
 include $(call all-named-subdir-makefiles,$(hardware_modules))
+
+endif
