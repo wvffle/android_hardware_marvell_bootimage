54eb8e95361d82ba7a22c61c50304343da0d5d94
Nemirtingas <nanaki89@hotmail.fr>
2018-09-14 12:27:23 +0200

    Marvell source

--- modules/power/power.c
+++ modules/power/power.c
@@ -19,22 +19,232 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-#define LOG_TAG "Legacy PowerHAL"
+#define LOG_TAG "PowerHAL"
 #include <utils/Log.h>
 
 #include <hardware/hardware.h>
 #include <hardware/power.h>
 
-static void power_init(struct power_module *module)
+#define POWERHALCFG "/etc/powerhal.conf"
+
+struct power_cpu_info
 {
+	char path[256];
+	char runtime_path[256];
+	int  control_fd;
+	struct power_cpu_info *next;
+	struct power_cpu_info *prev;
+};
+
+static struct power_cpu_info *devices = NULL;
+static struct power_cpu_info *last_device = NULL;
+
+int add_to_list( const char* buffer, int control_fd )
+{
+	struct power_cpu_info *device;
+	struct power_cpu_info *old_last_device;
+
+	device = (struct power_cpu_info*)malloc(sizeof(struct power_cpu_info));
+	if( device )
+	{
+		memset(device, 0, sizeof(struct power_cpu_info));
+		strncpy(device->path, buffer, 256);
+		strncpy(device->runtime_path, buffer, 256);
+		strncat(device->runtime_path, "/runtime_status", 256);
+		device->control_fd = control_fd;
+		if( devices )
+		{
+			old_last_device = last_device;
+			last_device->next = device;
+			last_device = device;
+			last_device->prev = old_last_device;
+		}
+		else
+		{
+			last_device = device;
+			devices = device;
+		}
+
+		return 0;
+	}
+	else
+	{
+		ALOGE("malloc failed for creating new node");
+	}
+	return -1;
 }
 
-static void power_set_interactive(struct power_module *module, int on)
+int is_pm_supported( const char *control, const char *runtime )
 {
+	char buf[12];
+	int fd;
+	if( access(control, 0) < 0 )
+	{
+		ALOGW("%s doesn't exist\n", control);
+		return -1;
+	}
+	fd = open(runtime, 0);
+	if( fd < 0 )
+	{
+		ALOGW("Error opening %s: %s\n", runtime, strerror(errno));
+		return -1;
+	}
+	if( read(fd, buf, sizeof(buf)) < 0 )
+	{
+		ALOGW("Error reading %s: %s\n", runtime, strerror(errno));
+		close(fd);
+		return -1;
+	}
+	if( !strncmp(buf, "unsupported", 11) )
+	{
+		ALOGW("%s is unsupported\n", runtime);
+		close(fd);
+		return -1;
+	}
+	close(fd);
+	return 0;
 }
 
-static void power_hint(struct power_module *module, power_hint_t hint,
-                       void *data) {
+void pm_set_for_device( struct power_cpu_info *device, int on )
+{
+	if( on )
+	{
+		if( write(device->control_fd, "on", 2) < 0 )
+		{
+			ALOGE("Error writing %s to %s: %s\n", "on", device->path, strerror(errno));
+			return;
+		}
+		ALOGD("write on done\n");
+		return;
+	}
+	if( write(device->control_fd, "auto", 4) >= 0 )
+	{
+		ALOGD("write auto done\n");
+	}
+	ALOGE("Error writing %s to %s: %s\n", "auto", device->path, strerror(errno));
+}
+
+void wait_for_set_complete(const char* runtime_path, int on )
+{
+	int fd;
+	char buf[12];
+	int retry = 1000;
+	const char *state;
+	int state_len;
+
+	while( retry-- > 0 )
+	{
+		fd = open(runtime_path, 0);
+		if( fd < 0 )
+		{
+			ALOGE("Error opening %s: %s\n", runtime_path, strerror(errno));
+			return;
+		}
+		if( read(fd, buf, sizeof(buf)) < 0 )
+		{
+			close(fd);
+			ALOGE("Error reading %s: %s\n", runtime_path, strerror(errno));
+		}
+		else
+		{
+			close(fd);
+			if( !strncmp(buf, "unsupported", 11) )
+			{
+				ALOGE("%s is unsupported\n", runtime_path);
+				break;
+			}
+			if( on )
+			{
+				state = "active";
+				state_len = 6;
+			}
+			else
+			{
+				state = "suspended";
+				state_len = 9;
+			}
+			if( !strncmp(buf, state, state_len) )
+				break;
+
+			usleep(1000);
+		}
+	}
+	if( retry <= 0 )
+		ALOGW("DEVICE SUSPEND/RESUME FAILED!\n");
+	ALOGD("wait_for_set_complete done, status is %s", buf);
+}
+
+static void power_init(__attribute__((unused)) struct power_module *module)
+{
+	FILE *conf = fopen(POWERHALCFG, "r");
+	int fd;
+	char buffer[256];
+	char control[256];
+	char runtime[256];
+	if( !conf )
+	{
+		ALOGW("%s does not exist! No devices for early suspend/late resume", POWERHALCFG);
+		return;
+	}
+	while( fgets(buffer, 256, conf) )
+	{
+		memset(control, 0, sizeof(control));
+		if( buffer[0] != '#' && buffer[0] != '\n' && buffer[0] != ' ' )
+		{
+			strncpy(control, buffer, 256);
+			strncat(control, "/control", 256);
+			strncpy(runtime, buffer, 256);
+			strncat(runtime, "/runtime_status", 256);
+			if( is_pm_supported(control, runtime) )
+			{
+				ALOGE("device %s do not support pm\n", buffer);
+				continue;
+			}
+			fd = open(control, 2);
+			if( fd >= 0 )
+			{
+				ALOGD("add device %s to list\n", buffer);
+				if( add_to_list(buffer, fd) )
+				{
+					ALOGE("add device %s to list failed\n", buffer);
+				}
+			}
+			else
+			{
+				ALOGE("Error opening %s: %s\n", control, strerror(errno));
+			}
+		}
+	}
+	fclose(conf);
+}
+
+static void power_set_interactive(__attribute__((unused)) struct power_module *module, int on)
+{
+	struct power_cpu_info *i;
+
+	ALOGD("power_set_interactive called with on %d\n", on);
+	if( on )
+	{
+		for( i = last_device; i; i = i->prev )
+		{
+			pm_set_for_device(i, on);
+		}
+	}
+	else
+	{
+		for( i = devices; i; i = i->next )
+		{
+			pm_set_for_device(i, 0);
+		}
+	}
+	for( i = last_device; i; i = i->prev )
+	{
+		wait_for_set_complete(i->runtime_path, on);
+	}
+}
+
+static void power_hint(__attribute__((unused)) struct power_module *module, power_hint_t hint,
+                       __attribute__((unused)) void *data) {
     switch (hint) {
     default:
         break;
@@ -51,8 +261,8 @@ struct power_module HAL_MODULE_INFO_SYM = {
         .module_api_version = POWER_MODULE_API_VERSION_0_2,
         .hal_api_version = HARDWARE_HAL_API_VERSION,
         .id = POWER_HARDWARE_MODULE_ID,
-        .name = "Default Power HAL",
-        .author = "The Android Open Source Project",
+        .name = "Marvell Power HAL",
+        .author = "Nemirtingas",
         .methods = &power_module_methods,
     },
 
