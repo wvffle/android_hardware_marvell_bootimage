58a5fb7aaa705ce01be9ee60a0253ec3f2774d78
Nemirtingas <nanaki89@hotmail.fr>
2018-11-09 09:53:18 +0100

    Added some compilation flags

--- mkbootimg/Android.mk
+++ mkbootimg/Android.mk
@@ -1,8 +1,14 @@
+ifeq ($(BOARD_PROVIDES_MKBOOTIMG),true)
+ifeq ($(BOARD_MKBOOTIMG_MRVL),true)
+
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../include
 LOCAL_SRC_FILES := mkbootimg.c
 LOCAL_STATIC_LIBRARIES := libmincrypt
-LOCAL_MODULE := mkbootimg-pxa
+LOCAL_MODULE := mkbootimg
 include $(BUILD_HOST_EXECUTABLE)
+
+endif
+endif
